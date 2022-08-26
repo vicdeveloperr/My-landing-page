55cf842a32789e44f2a69eb77a5e4c3d1a31ad00
vickyflow <vickyflow6@gmail.com>
2022-08-26 14:06:45 -0400

    primer push

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'oscuro': '#111',
+        'azul': '#3a00ff',
+        'claro': '#f2f2f2',
+        'amarilloClaro': '#ffd429',
+        'amarilloIntenso': '#ffba14'
+      },
+      boxShadow: {
+        'sombraBotones': '0px 0px 17px 4px rgba(58,0,255,0.24)'
+      },
+      screens: {
+      '450': '450px',
+      '600': '600px',
+      '700': '700px',
+      'sm': '640px',
+      'md': '768px',
+      '850': '850px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+      },
+    },
+  },
+  plugins: [],
+}
