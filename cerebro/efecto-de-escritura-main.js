c4431502c16825ec96a32c6f5fbae5c39bfdff61
vickyflow <vickyflow6@gmail.com>
2022-08-26 15:07:34 -0400

    segundo push

--- cerebro/efecto-de-escritura-main.js
+++ cerebro/efecto-de-escritura-main.js
@@ -5,10 +5,10 @@ const typed = new Typed('.typed', {
 	],
 
 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 70, // Velocidad en mlisegundos para poner una letra,
+	typeSpeed: 53, // Velocidad en mlisegundos para poner una letra,
 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
 	backSpeed: 40, // Velocidad en milisegundos para borrrar una letra,
-	smartBackspace: false, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
 	loop: false, // Repetir el array de strings
