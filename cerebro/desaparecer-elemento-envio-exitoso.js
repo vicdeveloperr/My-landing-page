ff4498d38fc8bc26beb68e1a7af2751f9b5427ca
vickyflow <vickyflow6@gmail.com>
2022-08-27 12:38:35 -0400

    prueba de envio de formulario por ajax

--- cerebro/desaparecer-elemento-envio-exitoso.js
+++ cerebro/desaparecer-elemento-envio-exitoso.js
@@ -1,3 +1,20 @@
 setTimeout( a => {
 	$("#envioExitoso").fadeOut();
 }, 4000);
+
+document.querySelector("#formulario-contacto").addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  let myForm = document.getElementById("#formulario-contacto");
+  let formData = new FormData(myForm);
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  })
+    .then(() => {
+    	alert("Envio Éxitoso");
+    });
+    .catch((error) => alert(error));
+};
